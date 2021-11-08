fedd7b35531c4f56f7eedee7996cb8ba722b11d4
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-11-08 09:18:07 +0000

    Successfully console log headlines

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const api_URL = 'https://content.guardianapis.com/search?page=1&api-key=test&show-fields=body,headline,thumbnail'
+const guardianApi_URL = 'https://content.guardianapis.com/search?page=1&api-key=test&show-fields=body,headline,thumbnail'
 
+const healdlines = document.getElementById("headL");
+
+getHeadlines(guardianApi_URL);
+
+function getHeadlines(URL) {
+  fetch(URL)
+  .then(dataStream => {
+    return dataStream.json();
+  })
+  .then(data => {
+    for (let i = 0; i < data.response.results.length; i++) {
+      console.log(data.response.results[i].fields.headline);
+    }
+  })
+}
+
+
+// .then(list => { 
+//   list.forEach(element => {
+//     allNoteEl.innerHTML +=
+//     `<div>${element.noteText}</div><br />`;
+//   })
